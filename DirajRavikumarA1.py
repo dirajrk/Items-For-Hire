6340c4aff1dae45d0c45b1a2d1d567c09be91c0b
Diraj Ravikumar <dirajravikumar@my.jcu.edu.au>
2016-04-03 10:50:02 +0800

    Modified .py file.

--- DirajRavikumarA1.py
+++ DirajRavikumarA1.py
@@ -1,7 +1,14 @@
 #By Diraj Ravikumar (13255244)
 
-print("Welcome to Items for Hire by Diraj Ravikumar")
+print("Items for Hire by Diraj Ravikumar")
 
 import csv
 
 cr = csv.reader(open("items.csv","rb"))
+
+print("Menu:")
+print("(L)ist all items")
+print("(H)ire an item")
+print("(R)eturn an item")
+print("(A)dd new item to stock")
+print("(Q)uit")
