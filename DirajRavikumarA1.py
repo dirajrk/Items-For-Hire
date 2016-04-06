279805cbb5c70c8fde2bab64c21f8e9d216dfae0
Diraj Ravikumar <dirajravikumar@my.jcu.edu.au>
2016-04-06 12:10:52 +0800

    Added a few lines.

--- DirajRavikumarA1.py
+++ DirajRavikumarA1.py
@@ -2,13 +2,39 @@
 
 print("Items for Hire by Diraj Ravikumar")
 
-import csv
+items_hire = open("items.csv","a")
 
-cr = csv.reader(open("items.csv","rb"))
-
+num_lines = sum(1 for line in open('items.csv'))
+print("\n{} items loaded from items.csv".format(num_lines))
+print("\n")
 print("Menu:")
 print("(L)ist all items")
 print("(H)ire an item")
 print("(R)eturn an item")
 print("(A)dd new item to stock")
 print("(Q)uit")
+
+choice = input()
+
+while True:
+    if choice == 'Q' or choice == 'q':
+        print("Finished")
+        break
+    elif choice == 'L' or choice == 'l':
+        print("L")
+    elif choice == 'H' or choice == 'h':
+        print("H")
+    elif choice == 'R' or choice == 'r':
+        print("R")
+    elif choice == 'A' or choice == 'a':
+        print("A")
+    else:
+        print("Invalid Option!")
+        print("Menu:")
+        print("(L)ist all items")
+        print("(H)ire an item")
+        print("(R)eturn an item")
+        print("(A)dd new item to stock")
+        print("(Q)uit")
+
+        choice = input()
