47358700826599b1ccf901b22062b153f7ad71fa
Diraj Ravikumar <dirajravikumar@my.jcu.edu.au>
2016-04-11 14:32:15 +0800

    Modified to list items as shown in the Word document and added a few comments.

--- DirajRavikumarA1.py
+++ DirajRavikumarA1.py
@@ -1,11 +1,14 @@
 #By Diraj Ravikumar (13255244)
 
-print("Items for Hire by Diraj Ravikumar")
+print("Items for Hire by Diraj Ravikumar\n")
 
-items_hire = open("items.csv","a")
+import csv
 
-num_lines = sum(1 for line in open('items.csv'))
-print("\n{} items loaded from items.csv".format(num_lines))
+items_hire = open("items.csv","r") #opens the .csv file
+items_hire_reader = csv.DictReader(items_hire,fieldnames=['itemName','desc','price','hire']) #used to read the lines in csv and separate them by their commas
+
+num_lines = sum(1 for line in open('items.csv')) #counts the number of items present in items.csv
+print("\n{} items loaded from items.csv".format(num_lines)) #shows the number of items present in items.csv
 print("\n")
 print("Menu:")
 print("(L)ist all items")
@@ -14,14 +17,23 @@ print("(R)eturn an item")
 print("(A)dd new item to stock")
 print("(Q)uit")
 
-choice = input()
+choice = input(">>>")
 
 while True:
     if choice == 'Q' or choice == 'q':
         print("Finished")
         break
     elif choice == 'L' or choice == 'l':
-        print("L")
+        for i in items_hire_reader:
+            print(i['itemName'],'(',i['desc'],')','= $',i['price'],i['hire'])
+        print("\n")
+        print("Menu:")
+        print("(L)ist all items")
+        print("(H)ire an item")
+        print("(R)eturn an item")
+        print("(A)dd new item to stock")
+        print("(Q)uit")
+        choice = input(">>>")
     elif choice == 'H' or choice == 'h':
         print("H")
     elif choice == 'R' or choice == 'r':
@@ -37,4 +49,4 @@ while True:
         print("(A)dd new item to stock")
         print("(Q)uit")
 
-        choice = input()
+        choice = input(">>>")
