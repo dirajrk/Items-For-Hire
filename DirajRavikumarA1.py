928d2fc843714f0dd172c378089f0e6e98b6f010
Diraj Ravikumar <dirajravikumar@my.jcu.edu.au>
2016-04-18 16:45:09 +0800

    Revamped program for Assignment 1 which does not involve "import csv".

--- DirajRavikumarA1.py
+++ DirajRavikumarA1.py
@@ -1,68 +1,67 @@
-#By Diraj Ravikumar (13255244)
+"""
+|=============================|
+|By Diraj Ravikumar (13255244)|
+|=============================|
+"""
 
-print("Items for Hire by Diraj Ravikumar\n")
-
-import csv
-
-items_hire = open("items.csv","r") #opens the .csv file
-items_hire_reader = csv.DictReader(items_hire,fieldnames=['itemName','desc','price','hire']) #used to read the lines in csv and separate them by their commas
-#items_hire_writer=csv.writer(items_hire)
+print("Items for Hire - by Diraj Ravikumar\n")
 
 num_lines = sum(1 for line in open('items.csv')) #counts the number of items present in items.csv
+print("{} items loaded from items.csv".format(num_lines)) #shows the number of items present in items.csv
+
+menu = ("\nMenu: \n(L)ist all items \n(H)ire an item \n(A)dd new item to stock \n(Q)uit")
+print(menu)
+choice = input(">>>")
 
 def list_items(): #A function to load items from items.csv
     """
-    Pseudo code coming soon.
+    Function list_items()
+
+    item_count = 0
+    items_hire = open items.csv
     """
     item_count = 0
-    for i in items_hire_reader:
-        if i['hire'] == 'out':
-            out = "*"
-            print(item_count, ' - ', i['itemName'], '(', i['desc'], ')', '= $', i['price'], out)
-        else:
-            print(item_count, ' - ', i['itemName'], '(', i['desc'], ')', '= $', i['price'])
+    with open('items.csv') as file:
+        item_lines_list = file.readlines()
+    for line in item_lines_list:
+        name, desc, price, hire = line.split(',')
+        print(item_count, " - ", name," (", desc, ") ", " = $", price, hire, end="")
         item_count += 1
 
+def hire_items(): #A function to hire items from items.csv
+    """
 
-print("{} items loaded from items.csv".format(num_lines)) #shows the number of items present in items.csv
-
-print("\nMenu:")
-print("(L)ist all items")
-print("(H)ire an item")
-print("(R)eturn an item")
-print("(A)dd new item to stock")
-print("(Q)uit")
-
-choice = input(">>>")
+    """
+    with open('items.csv') as file:
+        item_lines_list = file.readlines()
+    for line in item_lines_list:
+        name, desc, price, hire = line.split(',')
+        print(name," (", desc, ") ", " = $", price, end="")
 
 while True:
-    if choice == 'Q' or choice == 'q':
+    if choice == 'Q' or choice == 'q': #if user chooses to quit the program, it shows the number of items saved and breaks the program
         print("\n{} items saved to items.csv".format(num_lines))
         break
-    elif choice == 'L' or choice == 'l':
-        print("All items on file (* indicates item is currently out):")
-        list_items() #Calls the items load function
-        print("\n")
-        print("Menu:")
-        print("(L)ist all items")
-        print("(H)ire an item")
-        print("(R)eturn an item")
-        print("(A)dd new item to stock")
-        print("(Q)uit")
-        choice = input(">>>")
-    elif choice == 'H' or choice == 'h':
-        print("H")
-    elif choice == 'R' or choice == 'r':
-        print("R")
-    elif choice == 'A' or choice == 'a':
-        print("A")
-    else:
-        print("Invalid Option!")
-        print("Menu:")
-        print("(L)ist all items")
-        print("(H)ire an item")
-        print("(R)eturn an item")
-        print("(A)dd new item to stock")
-        print("(Q)uit")
+    elif choice == 'L' or choice == 'l': #list all the items
 
+        print("All items on file (* indicates item is currently out):\n")
+        list_items() #Calls the items load function
+
+        print("\n",menu) #prints out the menu
+        choice = input(">>>")
+
+    elif choice == 'H' or choice == 'h': #hire an item
+        hire_items()
+
+        print(menu)
+    elif choice == 'R' or choice == 'r': #return an item back
+        print("R")
+
+        print(menu)
+    elif choice == 'A' or choice == 'a': #add a new item to stock
+        print(menu)
+    else: #if the user chooses an option that is not available from the menu
+        print("Invalid Option!")
+
+        print(menu)
         choice = input(">>>")
